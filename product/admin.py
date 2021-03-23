477d38d8875b944c72cf1c53f320ae64db69c92f
jamshid1598 <dovurovjamshid95@gmail.com>
2021-03-23 11:54:47 +0500

    products and category data structure created in the models.py

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from tinymce.widgets import TinyMCE
+from .models import (
+    Category,
+    Product,
+    ProductImage,
+)
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display       = ()
+    list_display_links = ()
+    list_editable      = ()
+    search_fields      = ()
+    ordering           = ()
+
+    prepopulated_fields = {'slug' : ('name',)}
+
+    fieldsets = (
+        ('Category Info', {
+            "fields": (
+                
+            ),
+        }),
+    )    
+admin.site.register(Category, CategoryAdmin)
+
+
+
+class ProductImageAdmin(admin.TabularInline):
+    model   = ProductImage
+    extra   = 1
+    max_num = 4
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdmin,]
+
+    list_display       = ()
+    list_display_links = ()
+    list_editable      = ()
+    search_fields      = ()
+    ordering           = ()
+
+    prepopulated_fields = {'slug' : ('name',)}
+
+    fieldsets = (
+        ('Proruct Info', {
+            "fields": (
+                
+            ),
+        }),
+    )    
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 
 class Category(models.Model):
+    image = models.ImageField(upload_to='category-image/', blank=True, null=True)
+
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=300)
 
@@ -21,4 +23,21 @@ class Product(models.Model):
     product_model = models.CharField(max_length=100)
 
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name="Maxsulot"
+        verbose_name_plural="Maxsulotlar"
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
+
+    image = models.ImageField(upload_to="product-image/")
+    caption = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.pk
     
